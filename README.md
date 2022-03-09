06842647e1a5130b903fb74c101f4c47c140aa5b
Rob Currie <robert.currie@gmail.com>
2022-03-09 12:39:16 +0000

    Initial commit of ceremony and empty uk

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ph
+Rate pharmaceuticals based on their prevelance in the formulary of non-US OECD countries with better health outcomes than the US.
---
+++ uk.py
@@ -0,0 +1,5 @@
+import requests
+import PyPDF2 
+
+# https://www.england.nhs.uk/wp-content/uploads/2017/04/NHS-England-drugs-list-v16.1-Oct-2021-March-2022.pdf
+print("hello world")
